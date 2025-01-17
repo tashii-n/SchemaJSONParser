229e79780c3caee52c0f9e747f826dd1ee34c138
unknown <tashinamgay9876@gmail.com>
2025-01-17 20:25:14 +0600

    Added Audit model to Admin

--- schema_json_parser/settings.py
+++ schema_json_parser/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
-
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
